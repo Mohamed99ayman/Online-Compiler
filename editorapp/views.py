33785834f960a9c4435775eb0edbdadb1b9df719
Mohamed Ayman <44615609+Mohamed99ayman@users.noreply.github.com>
2020-08-28 17:44:41 +0200

    Update views.py
    absolute path

--- editorapp/views.py
+++ editorapp/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponseForbidden,HttpResponse
 from django.shortcuts import render, redirect
@@ -7,10 +8,14 @@ import subprocess
 import sys
 
 
-
-compiler={'CPP':'g++','JAVA':'javac','C':'gcc','Python':'python -m py_compile'}
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+gcomp='g++ -o '+(os.path.join(BASE_DIR,'a.out'))
+gccomp='gcc -o '+(os.path.join(BASE_DIR,'a.out'))
+compiler={'CPP':gcomp,'JAVA':'javac','C':gccomp,'Python':'python -m py_compile'}
 code_extent={'CPP':'cpp','JAVA':'java','C':'c','Python':'py'}
-outputcomp={'CPP':'./a.out','JAVA':'java TestClass ','C':'./a.out','Python':'python code.py'}
+aftercomp={'CPP':'a.out','JAVA':' TestClass ','C':'a.out','Python':'code.py'}
+beforecomp={'CPP':' ','JAVA':'java -cp ','C':' ','Python':'python '}
+
 
 def home(request):
     return render(request,"editorapp/index.html")
@@ -23,35 +28,36 @@ def runcode(request):
         inputtext=request.POST['input']
         language=request.POST['lang']
         data = {
-			'source': source,
+                        'source': source,
             'input':inputtext,
             'lang':language
-		}
+                }
         if language=='JAVA' and source.find('class TestClass ')==-1:
             return HttpResponse(json.dumps("Class name chaged \nRename the class to TestClass"),content_type='application/json')
         cmd=""
         # check if there is input
         if inputtext:
-            f=open("input.txt",'w')
+            f=open((os.path.join(BASE_DIR,"input.txt")),'w')
             f.write(inputtext)
             f.close()
-            cmd=" <input.txt"
-        
+            cmd=" <"+(os.path.join(BASE_DIR,"input.txt"))
+
         # create file to compile
 
-        f = open("code."+code_extent[language],"w")
+        f = open((os.path.join(BASE_DIR, 'code.'+code_extent[language])),'w')
         f.write(source)
         f.close()
         compilation_error=""
 
         # try compiling the code
         try:
-            subprocess.check_output(compiler[language]+" code."+code_extent[language], stderr=subprocess.PIPE,shell=True)
+            subprocess.check_output( compiler[language]+' '+(os.path.join(BASE_DIR,"code."+code_extent[language])), stderr=subprocess.PIPE,shell=True)
         except subprocess.CalledProcessError as e:
             compilation_error= e.stderr.decode(sys.getfilesystemencoding())
 
         if not compilation_error:
-            p=subprocess.Popen(outputcomp[language]+cmd+" >out.txt",shell=True)
+            p=subprocess.Popen(beforecomp[language]+os.path.join(BASE_DIR,aftercomp[language])+cmd+" >"+ (os.path.join(BASE_DIR,"out.txt")),shell=True)
+            print(beforecomp[language]+os.path.join(BASE_DIR,aftercomp[language])+cmd+" >out.txt")
             try:
                 p.wait(10)
             except subprocess.TimeoutExpired:
@@ -59,7 +65,8 @@ def runcode(request):
                 output="Process was terminated as it took longer than 10 seconds, was your code expecting an input?"
                 data=json.dumps(output)
                 return HttpResponse(data,content_type='application/json')
-            output=open('out.txt','r').read()
+            output=open((os.path.join(BASE_DIR,'out.txt')),'r').read()
+            print(BASE_DIR)
             data=json.dumps(output)
             return HttpResponse(data,content_type='application/json')
 
@@ -68,5 +75,3 @@ def runcode(request):
             return HttpResponse(data,content_type='application/json')
     else:
         return HttpResponseForbidden()
-
-            
